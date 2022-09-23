ecbd22c0589720f53e62c96a2ae05084fa9a4814
tracyneproski <tracyneproski@gmail.com>
2022-09-22 21:19:52 -0500

    Change word_length variable for clarity

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -27,7 +27,7 @@ def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
-  word_list.reduce(Hash.new()) { |memo, word_length| 
+  word_list.reduce(Hash.new()) { |memo, word| 
     word_list.select { |word| memo[word] = word.length() }
     return memo
   }
